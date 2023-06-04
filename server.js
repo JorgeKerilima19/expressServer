ddfdc301228a3776ada84b80f5dd688cd3fd1ba1
JorgeMioska <quispechilojorge@gmail.com>
2023-06-03 23:34:55 -0500

    two ways to send a file

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 5500;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send('Hello');
+  //   res.sendFile("./pages/index.html", { root: __dirname });
+  res.sendFile(path.join(__dirname, "pages", "index.html"));
 });
 
 app.listen(PORT, () => {
