f16f2bfeb4059f4b9c525ad25f89e8894d4b074b
JorgeMioska <quispechilojorge@gmail.com>
2023-06-05 05:26:59 -0500

    middleware intro2

--- server.js
+++ server.js
@@ -10,12 +10,22 @@ app.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "pages", "index.html"));
 });
 app.get("/mainPage(.html)?", (req, res) => {
-  //   res.sendFile("./pages/index.html", { root: __dirname });
   res.sendFile(path.join(__dirname, "pages", "mainPage.html"));
 });
-app.get("/*(.html)?", (req, res) => {
-  //   res.sendFile("./pages/index.html", { root: __dirname });
-  res.status(404).sendFile(path.join(__dirname,"pages","404.html"));
+
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("hello requested");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello Loaded");
+  }
+);
+
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "pages", "404.html"));
 });
 
 app.listen(PORT, () => {
