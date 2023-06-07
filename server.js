785f46e1827d135545eddc20240957b159052825
JorgeMioska <quispechilojorge@gmail.com>
2023-06-07 04:11:26 -0500

    CORS introduction

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
+const { callbackify } = require("util");
 
 const PORT = process.env.PORT || 5500;
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
+
+const allowedList = [
+  "http://www.google.com",
+  "http://www.mysite.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:5500",
+];
+
+const corsOption = {
+  origin: (origin, callback) => {
+    if (allowedList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOption));
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("^/$|/index(.html)?", (req, res) => {
   //   res.sendFile("./pages/index.html", { root: __dirname });
